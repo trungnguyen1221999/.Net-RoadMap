292756ac1141aad0abe33057553f470d3f35fbdd
Trung Nguyen <trungnguyen1221999@gmail.com>
2026-05-06 10:06:46 +0300

    inner join

---
+++ C# Mastery From Professional to Advanced Developer/1.1. Working with advanced LINQ queries/program.cs
@@ -0,0 +1,62 @@
+var students = new[]
+{
+    new
+    {
+        Id = 1,
+        Name = "Alice",
+        Age = 20,
+    },
+    new
+    {
+        Id = 2,
+        Name = "Bob",
+        Age = 22,
+    },
+    new
+    {
+        Id = 3,
+        Name = "Charlie",
+        Age = 21,
+    },
+    new
+    {
+        Id = 4,
+        Name = "David",
+        Age = 23,
+    },
+    new
+    {
+        Id = 5,
+        Name = "Eve",
+        Age = 20,
+    },
+};
+
+var scores = new[]
+{
+    new { studentId = 1, score = 85 },
+    new { studentId = 2, score = 90 },
+    new { studentId = 3, score = 85 },
+    new { studentId = 4, score = 85 },
+    new { studentId = 5, score = 85 },
+};
+
+// Inner Join
+
+var innerJoinQuery =
+    from student in students
+    join score in scores on student.Id equals score.studentId
+    select new
+    {
+        student.Id,
+        student.Name,
+        student.Age,
+        score.score,
+    };
+
+//Printing the results of the inner join
+
+foreach (var item in innerJoinQuery)
+{
+    Console.WriteLine($"Id: {item.Id}, {item.Name}, Age: {item.Age}, Score: {item.score}");
+}
