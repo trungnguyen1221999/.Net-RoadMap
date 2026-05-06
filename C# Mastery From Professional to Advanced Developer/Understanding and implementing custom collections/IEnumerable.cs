1a5b4947473e5cd1785e59283ecd562331f9b109
Trung Nguyen <trungnguyen1221999@gmail.com>
2026-05-06 14:27:35 +0300

    IEnumerable

---
+++ C# Mastery From Professional to Advanced Developer/Understanding and implementing custom collections/IEnumerable.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CustomCollection<T> : IEnumerable<T>
+{
+    private List<T> _items = new List<T>();
+
+    public void Add(T item) => _items.Add(item);
+
+    public void Remove(T item) => _items.Remove(item);
+
+    public int Count() => _items.Count();
+
+    public IEnumerator<T> GetEnumerator() => _items.GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        var collection = new CustomCollection<int>();
+        collection.Add(1);
+        collection.Add(2);
+        collection.Add(3);
+        foreach (var item in collection)
+        {
+            Console.WriteLine(item);
+        }
+        Console.WriteLine("------------------------------------------------");
+
+        Console.WriteLine("After removing 2:");
+        collection.Remove(2);
+
+        foreach (var item in collection)
+        {
+            Console.WriteLine(item);
+        }
+        Console.WriteLine("------------------------------------------------");
+        Console.WriteLine($"Count: {collection.Count()}");
+    }
+}
