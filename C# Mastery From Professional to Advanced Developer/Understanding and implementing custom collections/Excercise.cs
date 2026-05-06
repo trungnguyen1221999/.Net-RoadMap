fb7f9df2b24d07b3b1653b4953bad13bd6e381f3
Trung Nguyen <trungnguyen1221999@gmail.com>
2026-05-06 15:06:42 +0300

    Create Excercise.cs

---
+++ C# Mastery From Professional to Advanced Developer/Understanding and implementing custom collections/Excercise.cs
@@ -0,0 +1,3 @@
+//Create a Custom Collection that stores integers and does not allow duplicate values.
+//Add logging functionality when elements are added to or removed from the collection.
+//Write unit tests to verify the functionality of the Custom Collection.
