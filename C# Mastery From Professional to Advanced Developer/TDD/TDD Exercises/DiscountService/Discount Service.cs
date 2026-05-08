3352ad857e39073ca873fce1205963d33fa28221
Trung Nguyen <trungnguyen1221999@gmail.com>
2026-05-08 14:22:04 +0300

    simple TDD exercises

---
+++ C# Mastery From Professional to Advanced Developer/TDD/TDD Exercises/DiscountService/Discount Service.cs
@@ -0,0 +1,12 @@
+﻿public class DiscountService
+{
+    public decimal Discount { get; private set; }
+
+    public int CalculateDiscount(decimal price)
+    {
+        if (price < 1000000)
+            return 0;
+        else
+            return 5;
+    }
+}
---
+++ C# Mastery From Professional to Advanced Developer/TDD/TDD Exercises/DiscountServiceTest/DiscountServiceTest.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+
+public class DiscountServiceTest
+{
+    [Fact]
+    public void CalculateDiscount_WhenPriceLessThan1M_ReturnZero()
+    {
+        // Arrange
+        var discountService = new DiscountService();
+        // Act
+        var result = discountService.CalculateDiscount(999999);
+        // Assert
+
+        Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void CalculateDiscount_WhenPriceOver1M_ReturnFive()
+    {
+        // Arrange
+        var discountService = new DiscountService();
+        // Act
+        var result = discountService.CalculateDiscount(1000000);
+        // Assert
+        Assert.Equal(5, result);
+    }
+}
